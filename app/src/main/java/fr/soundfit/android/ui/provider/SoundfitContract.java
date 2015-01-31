f9c8215441f924c2a38ccd624d150de133709093
Donovan <donof43@gmail.com>
2015-01-31 17:51:35 +0100

    Tmp Commit

---
+++ app/src/main/java/fr/soundfit/android/ui/provider/SoundfitContract.java
@@ -0,0 +1,75 @@
+package fr.soundfit.android.ui.provider;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import fr.soundfit.android.BuildConfig;
+
+/**
+ * Created by Donovan on 31/01/2015.
+ */
+public class SoundfitContract {
+    private static final String TAG = SoundfitContract.class.getSimpleName();
+    private static final boolean DEBUG = BuildConfig.DEBUG;
+
+    public static String CONTENT_AUTHORITY = "fr.soundfit.android.provider";
+    protected static Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    protected static String VENDOR_NAME = "vnd.soundfit.android";
+
+    protected static final String PATH_SONG_SORT = "song_sort";
+
+    public interface Tables {
+        static final String SONG_SORT = "song_sort";
+    }
+
+
+    /**
+     * Song Sort
+     */
+    public interface SongSortColumns {
+        /** Default Column _ID */
+        /**
+         * Primary key id (INTEGER)
+         */
+        static final String ID_SONG = "id_song";
+        /**
+         * Title (TEXT)
+         */
+        static final String ID_TYPE = "id_type";
+
+    }
+    public static class SongSortTable implements SongSortColumns, BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_SONG_SORT).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + VENDOR_NAME + "." + PATH_SONG_SORT;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + VENDOR_NAME + "." + PATH_SONG_SORT;
+
+        public static class PROJ {
+            public static int _ID = 0;
+            public static int ID_SONG = 1;
+            public static int ID_TYPE = 2;
+
+            public static String[] COLS = new String[]{ //
+                    BaseColumns._ID,
+                    Tables.SONG_SORT + "." + SongSortColumns.ID_SONG, //
+                    Tables.SONG_SORT + "." + SongSortColumns.ID_TYPE, //
+            };
+        }
+
+        public static Uri buildUri() {
+            return CONTENT_URI;
+        }
+
+
+        public static Uri buildUriWithType(String type) {
+            return CONTENT_URI.buildUpon().appendPath(String.valueOf(type)).build();
+        }
+
+        public static String getType(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/fr/soundfit/android/ui/provider/SoundfitDatabase.java
@@ -0,0 +1,61 @@
+package fr.soundfit.android.ui.provider;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+import fr.soundfit.android.BuildConfig;
+
+/**
+ * Created by Donovan on 31/01/2015.
+ */
+public class SoundfitDatabase extends SQLiteOpenHelper {
+
+    private static final String TAG = SoundfitDatabase.class.getSimpleName();
+    private static final boolean DEBUG = BuildConfig.DEBUG;
+
+    public static String DATABASE_NAME = "soundfit_database";
+    public static int DATABASE_VERSION = 1;
+
+    public SoundfitDatabase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        if (DEBUG) {
+            Log.d(TAG, "SoundfitDatabase(context)");
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        createSongSortDatabase(sqLiteDatabase);
+    }
+
+    protected void createSongSortDatabase(SQLiteDatabase sqLiteDatabase) {
+        String newsTable = new StringBuilder() //
+                .append("CREATE TABLE ").append(SoundfitContract.Tables.SONG_SORT) //
+                .append(" ( '").append(BaseColumns._ID).append("' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '") //
+                .append(SoundfitContract.SongSortTable.ID_SONG).append("' INTEGER NOT NULL, '") //
+                .append(SoundfitContract.SongSortTable.ID_TYPE).append("' INTEGER NOT NULL ); '") //
+                .toString();
+        execSQL(newsTable, sqLiteDatabase);
+    }
+
+    private void execSQL(String request, SQLiteDatabase sqLiteDatabase){
+        sqLiteDatabase.execSQL(request);
+        if (DEBUG) {
+            Log.d(TAG, request);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        String deleteTable = new StringBuilder() //
+                .append("DROP TABLE ") //
+                .append(SoundfitContract.Tables.SONG_SORT).append(", ") //
+                .toString();
+        onCreate(sqLiteDatabase);
+    }
+
+
+}
