c23e18301eaaf960c5c07142a466268eced7dec9
Donovan <donof43@gmail.com>
2015-02-11 17:12:29 +0100

    Add SortFragment

--- app/src/main/java/fr/soundfit/android/ui/activity/PlaylistActivity.java
+++ app/src/main/java/fr/soundfit/android/ui/activity/PlaylistActivity.java
@@ -3,8 +3,15 @@ package fr.soundfit.android.ui.activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.deezer.sdk.model.Playlist;
+import com.deezer.sdk.model.Track;
+
+import java.util.List;
 
 import fr.soundfit.android.R;
+import fr.soundfit.android.ui.fragment.SortFragment;
 import fr.soundfit.android.ui.fragment.TrackCategoryPagerFragment;
 
 /**
@@ -15,7 +22,8 @@ public class PlaylistActivity extends GenericChildActivity {
     public static final String EXTRA_PLAYLIST_ID = "fr.soundfit.android.EXTRA_PLAYLIST_ID";
 
     private long mPlaylistId;
-    private Fragment mFragment;
+    private Fragment mMainFragment;
+    private Fragment mSortFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +38,32 @@ public class PlaylistActivity extends GenericChildActivity {
         String tag = TrackCategoryPagerFragment.TAG + mPlaylistId;
 
         final FragmentManager fm = getSupportFragmentManager();
-        mFragment = fm.findFragmentByTag(tag);
-        if (mFragment == null) {
-            mFragment =  TrackCategoryPagerFragment.newInstance(mPlaylistId);
-            fm.beginTransaction().add(R.id.container, mFragment, tag).commit();
+        mMainFragment = fm.findFragmentByTag(tag);
+        if (mMainFragment == null) {
+            mMainFragment =  TrackCategoryPagerFragment.newInstance(mPlaylistId);
+            fm.beginTransaction().add(R.id.container, mMainFragment, tag).commit();
         } else {
-            fm.beginTransaction().show(mFragment).commit();
+            fm.beginTransaction().show(mMainFragment).commit();
         }
+    }
+
+    public void displaySortFragment(Playlist playlist){
+        final FragmentManager fm = getSupportFragmentManager();
+        String tag = SortFragment.TAG;
+        mSortFragment = fm.findFragmentByTag(tag);
+        if (mSortFragment == null) {
+            mSortFragment =  SortFragment.newInstance(playlist);
+            fm.beginTransaction().add(R.id.container, mSortFragment, tag).addToBackStack(tag).commit();
+        } else {
+            fm.beginTransaction().show(mSortFragment).addToBackStack(tag).commit();
+        }
+    }
+
+    public void hideSortFragment(){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction trans = manager.beginTransaction();
+        trans.remove(mSortFragment);
+        trans.commit();
+        manager.popBackStack();
     }
 }
--- app/src/main/java/fr/soundfit/android/ui/fragment/TrackCategoryPagerFragment.java
+++ app/src/main/java/fr/soundfit/android/ui/fragment/TrackCategoryPagerFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.Toast;
 
 import com.deezer.sdk.model.Playlist;
 import com.deezer.sdk.model.Track;
@@ -12,12 +13,14 @@ import com.deezer.sdk.network.request.AsyncDeezerTask;
 import com.deezer.sdk.network.request.DeezerRequest;
 import com.deezer.sdk.network.request.DeezerRequestFactory;
 import com.deezer.sdk.network.request.event.JsonRequestListener;
+import com.melnykov.fab.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import fr.soundfit.android.R;
 import fr.soundfit.android.ui.activity.GenericActivity;
+import fr.soundfit.android.ui.activity.PlaylistActivity;
 import fr.soundfit.android.ui.adapter.TrackCategoryPagerAdapter;
 import fr.soundfit.android.ui.view.SlidingTabLayout;
 
@@ -26,7 +29,7 @@ import fr.soundfit.android.ui.view.SlidingTabLayout;
  * Package : fr.soundfit.android.ui.fragment
  * By Donovan on 05/02/2015.
  */
-public class TrackCategoryPagerFragment extends GenericFragment implements SlidingTabLayout.TabColorizer {
+public class TrackCategoryPagerFragment extends GenericFragment implements SlidingTabLayout.TabColorizer, View.OnClickListener {
 
     public static final String TAG = TrackCategoryPagerFragment.class.getSimpleName();
 
@@ -38,6 +41,7 @@ public class TrackCategoryPagerFragment extends GenericFragment implements Slidi
 
     protected SlidingTabLayout mSlidingTabLayout;
     protected ViewPager mViewPager;
+    protected FloatingActionButton mSortButton;
 
 
     public static TrackCategoryPagerFragment newInstance(long playlistId) {
@@ -51,7 +55,7 @@ public class TrackCategoryPagerFragment extends GenericFragment implements Slidi
 
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_pager;
+        return R.layout.fragment_pager_button;
     }
 
     @Override
@@ -59,7 +63,8 @@ public class TrackCategoryPagerFragment extends GenericFragment implements Slidi
         super.bindView(view);
         mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
         mSlidingTabLayout = (SlidingTabLayout) view.findViewById(R.id.sliding_tabs);
-
+        mSortButton = (FloatingActionButton) view.findViewById(R.id.fab);
+        mSortButton.setOnClickListener(this);
         mDeezerConnect = ((GenericActivity)getActivity()).getDeezerConnection();
     }
 
@@ -87,6 +92,14 @@ public class TrackCategoryPagerFragment extends GenericFragment implements Slidi
         }
     }
 
+    @Override
+    public void onClick(View view) {
+        if(getActivity() instanceof PlaylistActivity){
+            PlaylistActivity activity = (PlaylistActivity) getActivity();
+            activity.displaySortFragment(mPlaylist);
+        }
+    }
+
     private class TrackListener extends JsonRequestListener {
         @Override
         public void onResult(Object result, Object requestId) {
@@ -96,6 +109,7 @@ public class TrackCategoryPagerFragment extends GenericFragment implements Slidi
                 mSlidingTabLayout.setDistributeEvenly(true);
                 mSlidingTabLayout.setCustomTabColorizer(TrackCategoryPagerFragment.this);
                 mSlidingTabLayout.setViewPager(mViewPager);
+                mSortButton.setVisibility(View.VISIBLE);
             }
             catch (ClassCastException e) {
                 displayError(true);
